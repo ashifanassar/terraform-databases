53cf5411ecdb250e3c1c8c5900e0592f6263dc90
ashifanassar <ashifanassar@gmail.com>
2024-04-26 05:47:29 +0530

    databases commit

--- main.tf
+++ main.tf
@@ -5,22 +5,22 @@ module "mongodb" {
 }
 
 
-module "mysql" {
-    source      = "./vendor/modules/mysql/"
-    ENV = var.ENV
-    AZ = var.AZ
-}
+# module "mysql" {
+#     source      = "./vendor/modules/mysql/"
+#     ENV = var.ENV
+#     AZ = var.AZ
+# }
 
 
-module "redis" {
-    source      = "./vendor/modules/redis/"
-    ENV = var.ENV
-    AZ = var.AZ
-}
+# module "redis" {
+#     source      = "./vendor/modules/redis/"
+#     ENV = var.ENV
+#     AZ = var.AZ
+# }
 
 
-module "rabbitmq" {
-    source      = "./vendor/modules/rabbitmq/"
-    ENV = var.ENV
-    AZ = var.AZ
-}
+# module "rabbitmq" {
+#     source      = "./vendor/modules/rabbitmq/"
+#     ENV = var.ENV
+#     AZ = var.AZ
+# }
