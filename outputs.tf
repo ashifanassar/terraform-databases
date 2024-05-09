daf08f1a76045c5d8de20dc33b86100b85bd4def
ashifanassar <ashifanassar@gmail.com>
2024-05-09 23:22:10 +0530

    module

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "DOCDB_ENDPOINT" {
     value = module.documentdb.DOCDB_ENDPOINT
 }
 
-# output "REDIS_ENDPOINT" {
-#     value = module.redis.REDIS_ENDPOINT
-# }
+output "REDIS_ENDPOINT" {
+    value = module.redis.REDIS_ENDPOINT
+}
 
 # output "MYSQL_ENDPOINT" {
 #     value = module.mysql.MYSQL_ENDPOINT
